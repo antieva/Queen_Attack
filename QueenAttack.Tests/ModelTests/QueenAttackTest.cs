be7ee4b5d5a921d10f9de8f36a59dc6a49e16334
Guest User <Guest@epicodus-10.local>
2018-04-23 15:56:11 -0700

    Add initial files to set up MVC and MSTest.

---
+++ QueenAttack.Tests/ModelTests/QueenAttackTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueenAttackApp;
+
+namespace QueenAttack.Tests
+{
+  [TestClass]
+  public class QueenAttackTest
+  {
+    [TestMethod]
+    public void someFunction()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
