c3820087d82621c7d35ca5380d54a878cdd63899
Guest User <Guest@epicodus-10.local>
2018-04-24 08:52:01 -0700

    Update method to check diagonals on the board, write method to test them.

--- QueenAttack.Tests/ModelTests/QueenAttackTest.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTest.cs
@@ -7,38 +7,17 @@ namespace QueenAttackApp.Tests
   public class QueenAttackTest
   {
     [TestMethod]
-    public void CanAttackUpperRightDiagonal_QueenIn1AFigure7G_True()
+    public void ChackLeftToRightDownDiagonal_QueenIn3AFigure1C_True()
     {
         QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackUpperRightDiagonal(1,1,7,7));
+        Assert.AreEqual(true, testQueenAttack.ChackLeftToRightDownDiagonal(3,1,1,3));
     }
 
     [TestMethod]
-    public void CanAttackLowerLeftDiagonal_QueenIn8HFigure2B_True()
+    public void ChackRightToLeftDownDiagonal_QueenIn8HFigure2B_True()
     {
         QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackLowerLeftDiagonal(8,8,2,2));
-    }
-
-    [TestMethod]
-    public void CanAttackUpperLeftDiagonal_QueenIn3CFigure5A_True()
-    {
-        QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackUpperLeftDiagonal(3,3,5,1));//coordinates passed in a reversed manner;
-    }
-// Testing the same function on different diagonals.
-    [TestMethod]
-    public void CheckAllDiagonals_QueenIn3CFigure5A_True()
-    {
-        QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CheckAllDiagonals(3,3,5,1));
-    }
-
-    [TestMethod]
-    public void CheckAllDiagonals_QueenIn2EFigure5H_True()
-    {
-        QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CheckAllDiagonals(2,5,5,8));
+        Assert.AreEqual(true, testQueenAttack.ChackRightToLeftDownDiagonal(8,8,2,2));
     }
 
     [TestMethod]
@@ -46,6 +25,13 @@ namespace QueenAttackApp.Tests
     {
         QueenAttack testQueenAttack = new QueenAttack();
         Assert.AreEqual(true, testQueenAttack.CheckRowAndColumn(2,5,2,8));
+    }
+
+    [TestMethod]
+    public void CheckAll_QueenIn2EFigure5H_True()
+    {
+        QueenAttack testQueenAttack = new QueenAttack();
+        Assert.AreEqual(true, testQueenAttack.CheckAll(2,5,5,8));
     }
 
   }
--- QueenAttack/Models/QueenAttack.cs
+++ QueenAttack/Models/QueenAttack.cs
@@ -5,57 +5,22 @@ namespace QueenAttackApp
 {
   public class QueenAttack
   {
-    public bool CanAttackUpperRightDiagonal(int row1, int col1, int row2, int col2)
+    public bool ChackLeftToRightDownDiagonal(int row1, int col1, int row2, int col2)
     {
-        int tmp = col1 +1;
-        for (int i = row1 + 1; i <= 8; i++) {
-            if (tmp == 9) {
-              break;
-            }
-            if (i == row2 && tmp == col2) {
-              return true;
-            }
-            tmp++;
+        if (row1 + col1 == row2 + col2) {
+          return true;
         }
         return false;
     }
 
-    public bool CanAttackLowerLeftDiagonal(int row1, int col1, int row2, int col2)
+    public bool ChackRightToLeftDownDiagonal(int row1, int col1, int row2, int col2)
     {
-        int tmp = col1 - 1;
-        for (int i = row1 - 1; i >= 1; i--) {
-            if (tmp == 0) {
-              break;
-            }
-            if (i == row2 && tmp == col2) {
-              return true;
-            }
-            tmp--;
+        col1 = 9 - col1;
+        col2 = 9 - col2;
+        if (row1 + col1 == row2 + col2) {
+          return true;
         }
         return false;
-    }
-    public bool CanAttackUpperLeftDiagonal(int row1, int col1, int row2, int col2)
-    {
-        int tmp = col1 - 1;
-        for (int i = row1 + 1; i <= 8; i++) {
-            if (tmp == 0) {
-              break;
-            }
-            if (i == row2 && tmp == col2) {
-              return true;
-            }
-            tmp--;
-        }
-        return false;
-    }
-
-    public bool CheckAllDiagonals(int row1, int col1, int row2, int col2)
-    {
-        if (CanAttackUpperRightDiagonal(row1, col1, row2, col2) || CanAttackLowerLeftDiagonal(row1, col1, row2, col2) || CanAttackUpperLeftDiagonal(row1, col1, row2, col2) || CanAttackUpperLeftDiagonal(col1, row1, col2, row2)) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public bool CheckRowAndColumn(int row1, int col1, int row2, int col2)
@@ -68,7 +33,7 @@ namespace QueenAttackApp
 
     public bool CheckAll(int row1, int col1, int row2, int col2)
     {
-        if (CheckAllDiagonals(row1,col1,row2,col2) || CheckRowAndColumn(row1,col1,row2,col2)) {
+        if (ChackRightToLeftDownDiagonal(row1,col1,row2,col2) || ChackLeftToRightDownDiagonal(row1,col1,row2,col2) || CheckRowAndColumn(row1,col1,row2,col2)) {
           return true;
         } else {
           return false;
