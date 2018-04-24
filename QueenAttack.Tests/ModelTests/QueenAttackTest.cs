ea79e1db669822dc7b3dfe972a3380e9afb4f6ea
Eva Antipina <eva.antipina@gmail.com>
2018-04-23 22:29:36 -0700

    Add function to method all diagnals if the figure in the Queen reach, add test method for that function, add method to check row and column, add test method to test CheckRowAndColumn method.

--- QueenAttack.Tests/ModelTests/QueenAttackTest.cs
+++ QueenAttack.Tests/ModelTests/QueenAttackTest.cs
@@ -7,24 +7,46 @@ namespace QueenAttackApp.Tests
   public class QueenAttackTest
   {
     [TestMethod]
-    public void CanAttackUpperRightDiagonal_QueenInLeftLowerCorner_True()
+    public void CanAttackUpperRightDiagonal_QueenIn1AFigure7G_True()
     {
         QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackUpperRightDiagonal(2,3,6,7));
+        Assert.AreEqual(true, testQueenAttack.CanAttackUpperRightDiagonal(1,1,7,7));
     }
 
     [TestMethod]
-    public void CanAttackLowerLeftDiagonal_QueenInRightUpperCorner_True()
+    public void CanAttackLowerLeftDiagonal_QueenIn8HFigure2B_True()
     {
         QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackLowerLeftDiagonal(2,3,1,2));
+        Assert.AreEqual(true, testQueenAttack.CanAttackLowerLeftDiagonal(8,8,2,2));
     }
 
     [TestMethod]
-    public void CanAttackUpperLeftDiagonal_QueenInRightUpperCorner_True()
+    public void CanAttackUpperLeftDiagonal_QueenIn3CFigure5A_True()
     {
         QueenAttack testQueenAttack = new QueenAttack();
-        Assert.AreEqual(true, testQueenAttack.CanAttackUpperLeftDiagonal(1,8,4,5));
+        Assert.AreEqual(true, testQueenAttack.CanAttackUpperLeftDiagonal(3,3,5,1));//coordinates passed in a reversed manner;
     }
+// Testing the same function on different diagonals.
+    [TestMethod]
+    public void CheckAllDiagonals_QueenIn3CFigure5A_True()
+    {
+        QueenAttack testQueenAttack = new QueenAttack();
+        Assert.AreEqual(true, testQueenAttack.CheckAllDiagonals(3,3,5,1));
+    }
+
+    [TestMethod]
+    public void CheckAllDiagonals_QueenIn2EFigure5H_True()
+    {
+        QueenAttack testQueenAttack = new QueenAttack();
+        Assert.AreEqual(true, testQueenAttack.CheckAllDiagonals(2,5,5,8));
+    }
+
+    [TestMethod]
+    public void CheckRowAndColumn_QueenIn2EFigure2H_True()
+    {
+        QueenAttack testQueenAttack = new QueenAttack();
+        Assert.AreEqual(true, testQueenAttack.CheckRowAndColumn(2,5,2,8));
+    }
+
   }
 }
